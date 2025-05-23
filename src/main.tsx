a8f306d1755db1ddcb76b57ce07916f8ee55fe5c
HYUNFLIX <hyunnet@gmail.com>
2025-05-23 10:21:59 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+// BrowserRouter에 basename을 지정
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <BrowserRouter basename="/demian-website">
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>
 );
