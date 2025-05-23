c27f65858a57036b7d24d42929ccedcb756bdf46
HYUNFLIX <hyunnet@gmail.com>
2025-05-23 10:29:17 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
+// src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// BrowserRouter에 basename을 지정
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter basename="/demian-website">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
